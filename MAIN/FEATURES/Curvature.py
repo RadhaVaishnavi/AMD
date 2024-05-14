7a6d2b9203febf6eaca099ae63c4906bbdde5255
RadhaVaishnavi <84319477+RadhaVaishnavi@users.noreply.github.com>
2024-05-14 21:40:22 +0530

    Update Curvature.py

--- MAIN/FEATURES/Curvature.py
+++ MAIN/FEATURES/Curvature.py
@@ -1,1 +1,12 @@
 
+from scipy import ndimage
+import numpy as np
+import cv2
+
+def curvature_fea(img):
+    edge_horizont = ndimage.sobel(img, 0)
+    edge_vertical = ndimage.sobel(img, 1)
+    curvature = np.hypot(edge_horizont, edge_vertical)
+    curvature *= 255.0 / np.max(curvature)
+
+    return np.mean(curvature)
